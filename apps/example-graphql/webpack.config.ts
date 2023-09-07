80ac51f54e6f1b9497d73766daf245da76c525a0
marcus-sa <marcus-sa@outlook.com>
2023-09-07 20:41:39 +0200

    feat: add nx-webpack-plugin package

--- apps/example-graphql/webpack.config.ts
+++ apps/example-graphql/webpack.config.ts
@@ -1,6 +1,5 @@
 import { composePlugins, withNx } from '@nx/webpack';
-
-import { withDeepkit } from '../with-deepkit';
+import { withDeepkit } from '@deepkitx/nx-webpack-plugin';
 
 // eslint-disable-next-line import/no-default-export
 export default composePlugins(withNx(), withDeepkit());
