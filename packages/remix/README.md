8375762e3774dad1f95be34c39468b31a08bb5f5
marcus-sa <marcus-sa@outlook.com>
2023-09-08 11:42:19 +0200

    chore: trigger release

--- packages/remix/README.md
+++ packages/remix/README.md
@@ -1,11 +1,11 @@
-# deepkit-remix
+# remix
 
 This library was generated with [Nx](https://nx.dev).
 
 ## Building
 
-Run `nx build deepkit-remix` to build the library.
+Run `nx build remix` to build the library.
 
 ## Running unit tests
 
-Run `nx test deepkit-remix` to execute the unit tests via [Jest](https://jestjs.io).
+Run `nx test remix` to execute the unit tests via [Jest](https://jestjs.io).
--- packages/remix/jest.config.ts
+++ packages/remix/jest.config.ts
@@ -1,10 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'deepkit-remix',
+  displayName: 'remix',
   preset: '../../jest.preset.js',
+  testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/libs/deepkit-remix',
+  coverageDirectory: '../../coverage/libs/remix',
 };
