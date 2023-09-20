b9d19d15a40d5cf2e6b5062ad9f736b51683036d
marcus-sa <marcus-sa@outlook.com>
2023-09-20 16:17:37 +0200

    fix(graphql): re-export everything

--- packages/graphql/src/index.ts
+++ packages/graphql/src/index.ts
@@ -1,13 +1,7 @@
-export { graphql } from './lib/decorators';
-export {
-  TypesBuilder,
-  TypeNameRequiredError,
-  Parent,
-  Context,
-  ID,
-} from './lib/types-builder';
-export { SchemaBuilder, buildSchema } from './lib/schema-builder';
-export { GraphQLModule } from './lib/graphql.module';
-export { GraphQLConfig } from './lib/graphql-config';
-export { Driver } from './lib/driver';
-export { Resolvers } from './lib/resolvers';
+export * from './lib/decorators';
+export * from './lib/types-builder';
+export * from './lib/schema-builder';
+export * from './lib/graphql.module';
+export * from './lib/graphql-config';
+export * from './lib/driver';
+export * from './lib/resolvers';
