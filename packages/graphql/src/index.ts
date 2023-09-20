301823cea620070cc48830451e9b5de4bd954e86
marcus-sa <marcus-sa@outlook.com>
2023-09-20 14:59:59 +0200

    fix(graphql): export Resolvers

--- packages/graphql/src/index.ts
+++ packages/graphql/src/index.ts
@@ -10,3 +10,4 @@ export { SchemaBuilder } from './lib/schema-builder';
 export { GraphQLModule } from './lib/graphql.module';
 export { GraphQLConfig } from './lib/graphql-config';
 export { Driver } from './lib/driver';
+export { Resolvers } from './lib/resolvers';
